321ef3225ab87977aec4d66493e8c7127c580235
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2025-01-12 23:58:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 import time
 import os
 import tempfile
+from mwdblib import MWDB
 
 app = Flask(__name__)
 
@@ -23,37 +24,36 @@ def scan():
         return jsonify({"error": "hash_value is required"}), 400
 
     temp_file = tempfile.NamedTemporaryFile(delete=False)
-    file_path = temp_file.name
 
     headers = {
         'accept': 'application/octet-stream',
         'Authorization': f'Bearer {config_api_key}'
     }
     response = requests.get(f"{config_api_url}/file/{hash_value}/download", headers=headers, stream=True)
-    
-    if response.status_code != 200:
-        os.remove(file_path)
+
+    mwdb = MWDB(api_url=config_api_url, api_key=config_api_key)
+    file = mwdb.query_file(sha256)
+
+    try:
+        file_content = file.download()
+    except:
         return jsonify({
             "error": "Failed to download file",
             "status_code": response.status_code,
             "response_text": response.text
         }), 500
-    
-    for chunk in response.iter_content(chunk_size=8192):
-        if chunk:
-            temp_file.write(chunk)
-    
-    temp_file.close()
+    finally:
+        temp_file.close()
 
     analysis_date = int(time.time())
     analysis_results = {}
 
     for endpoint in API_ENDPOINTS:
-        with open(file_path, 'rb') as f:
-            files = {'file': (os.path.basename(file_path), f, 'application/octet-stream')}
+        with open(temp_file.name, 'rb') as f:
+            files = {'file': (os.path.basename(temp_file.name), f, 'application/octet-stream')}
             response = requests.post(endpoint["url"], files=files)
             if response.status_code != 200:
-                os.remove(file_path)
+                os.remove(temp_file.name)
                 return jsonify({
                     "error": f"Failed to scan file with {endpoint['name']}",
                     "status_code": response.status_code,
@@ -72,7 +72,7 @@ def scan():
         }
     ]
     
-    os.remove(file_path)
+    os.remove(temp_file.name)
     return jsonify(final_result)
 
 if __name__ == '__main__':
