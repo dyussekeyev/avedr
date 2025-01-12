1439ad9ea8bc335e89eaf648705dec90c9dac6a3
Askar Dyussekeyev <91150176+dyussekeyev@users.noreply.github.com>
2025-01-12 18:00:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/kvrt', methods=['GET'])
 def kvrt():
-    response = requests.get('http://kvrt:8000/scan')
-    return jsonify(response.json())
+    response = requests.get('http://127.0.0.1:8000/scan')
+    return jsonify({"kvrt": response.json()})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555)
